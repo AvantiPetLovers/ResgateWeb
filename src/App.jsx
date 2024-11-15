fc3211027a6243facdb564b478332fbe22b0823e
Ricardo <lrsj2003@gmail.com>
2024-11-15 18:38:27 -0300

    setup projeto

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import resgateLogo from './assets/react.svg'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='mx-auto'>
+      <div>
+        <img src={resgateLogo} className="logo react" alt="React logo" />
+      </div>
+
+      <h1>Resgate</h1>
+
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
