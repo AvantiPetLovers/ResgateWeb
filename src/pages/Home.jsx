7749884f11f63277fe452332573ee130ed57939e
Ricardo <lrsj2003@gmail.com>
2024-11-26 02:52:04 -0300

    card pet home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { useNavigate } from 'react-router-dom';
+import { findPets } from '../services/ApiService';
+import PetCard from '../components/PetCard';
 
 
 export default function Home() {
-    const [count, setCount] = useState(0)
     const navigate = useNavigate();
+    const [pets, setPets] = useState([])
 
+    // Busca os pets no banco de dados
+    useEffect(() => {
+      const listPets = async () => {
+        const { data } = await findPets();
+        setPets(data.slice(0, 4));
+      };
+      listPets();
+    }, []);
 
     return (
         <>
@@ -24,8 +34,24 @@ export default function Home() {
                 </div>
             </div>
 
-
-            
+            <div className="m-12 ">
+                <div className="flex justify-between mb-8">
+                    <div>
+                        <h1 className="font-bold text-xs ">Quer Adotar?</h1>
+                        <span className="font-bold text-2xl text-sky-950">Confira Nossos Pets</span>
+                    </div>
+                    <button
+                        onClick={() => navigate("/pet")}
+                        className="border border-sky-950 text-sky-950 font-semibold py-2 px-4 rounded-full hover:bg-sky-950 hover:text-white transition duration-300" >
+                        {'Ver mais >'}
+                    </button>
+                </div>
+                <div className="flex flex-wrap gap-2 mb-8">
+                    {pets.map((pet) => (
+                        <PetCard key={pet.id} pet={pet} />
+                    ))}
+                </div>
+            </div>
 
             {/* TODO: Issue 6 */}
             <div className="bg-blue-500 flex m-12 p-6 rounded h-[300px]">
