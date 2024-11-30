6ef0b6d17323e1f30eeeef1dc5f2c1b8b0873068
Ricardo <lrsj2003@gmail.com>
2024-11-29 23:25:13 -0300

    conflitos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import sobreImg from "../assets/sobre.png";
 import PetListLine from '../components/PetListLine';
 
 export default function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -34,29 +34,7 @@ export default function Home() {
         </div>
       </div>
 
-            <PetListLine title="Quer Adotar?" subTitle="Confira Nossos Pets" />
-
-      <div className="m-12 ">
-        <div className="flex justify-between mb-8">
-          <div>
-            <h1 className="font-bold text-xs ">Quer Adotar?</h1>
-            <span className="font-bold text-2xl text-sky-950">
-              Confira Nossos Pets
-            </span>
-          </div>
-          <button
-            onClick={() => navigate("/pet")}
-            className="border border-sky-950 text-sky-950 font-semibold py-2 px-4 rounded-full hover:bg-sky-950 hover:text-white transition duration-300"
-          >
-            {"Ver mais >"}
-          </button>
-        </div>
-        <div className="flex flex-wrap gap-2 mb-8">
-          {pets.map((pet) => (
-            <PetCard key={pet.id} pet={pet} />
-          ))}
-        </div>
-      </div>
+      <PetListLine title="Quer Adotar?" subTitle="Confira Nossos Pets" />
 
       <div className="bg-gray-100 m-12 p-10 rounded-lg shadow-lg grid md:grid-cols-2 gap-10 items-center">
         <div>
@@ -80,24 +58,24 @@ export default function Home() {
       </div>
 
       <div className="bg-blue-100 m-12 p-10 rounded-lg shadow-lg">
-  <h1 className="text-3xl font-extrabold text-blue-600 mb-6">
-    📞 Entre em Contato
-  </h1>
-  <p className="text-gray-700 text-lg mb-6">
-    Tem dúvidas ou quer ajudar nossos pets? Estamos aqui para você!
-  </p>
-  <ul className="text-gray-600 space-y-4">
-    <li>
-      ✉️ <strong>Email:</strong> contato@resgate20.com
-    </li>
-    <li>
-      📱 <strong>WhatsApp:</strong> (xx) xxxx-xxxx
-    </li>
-    <li>
-      📍 <strong>Endereço:</strong> Rua Amor aos Animais, nº 123, Sua Cidade, Seu Estado
-    </li>
-  </ul>
-</div>
+        <h1 className="text-3xl font-extrabold text-blue-600 mb-6">
+          📞 Entre em Contato
+        </h1>
+        <p className="text-gray-700 text-lg mb-6">
+          Tem dúvidas ou quer ajudar nossos pets? Estamos aqui para você!
+        </p>
+        <ul className="text-gray-600 space-y-4">
+          <li>
+            ✉️ <strong>Email:</strong> contato@resgate20.com
+          </li>
+          <li>
+            📱 <strong>WhatsApp:</strong> (xx) xxxx-xxxx
+          </li>
+          <li>
+            📍 <strong>Endereço:</strong> Rua Amor aos Animais, nº 123, Sua Cidade, Seu Estado
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
